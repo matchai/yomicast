53cd53353afb1f20ee8d8b58f0647256435a7008
Matan Kushner <hello@matchai.dev>
2025-05-26 16:55:54 +0900

    Start over using jmdict instead of jitendex

--- src/lib.ts
+++ src/lib.ts
@@ -1,16 +1,14 @@
 import { environment } from "@raycast/api";
-import { MDX } from "js-mdict";
 import fsSync from "node:fs";
 import path from "node:path";
 import os from "node:os";
 
 export const DICTIONARY_URL =
-  "https://github.com/stephenmk/stephenmk.github.io/releases/latest/download/jitendex-mdict.zip";
+  "https://github.com/stephenmk/stephenmk.github.io/releases/latest/download/jitendex-yomitan.zip";
 
-export const DOWNLOAD_PATH = path.join(
-  fsSync.mkdtempSync(path.join(os.tmpdir(), "jitendex-mdict-")),
-  "jitendex-mdict.zip",
-);
+export const TEMP_DIR = fsSync.mkdtempSync(path.join(os.tmpdir(), "jitendex-mdict-"));
+export const EXTRACTED_PATH = path.join(TEMP_DIR, "jitendex-yomitan");
+
 export const DICTIONARY_PATH = path.join(environment.supportPath, "jitendex-mdict.mdx");
 export const DB_PATH = path.join(environment.supportPath, "jitendex.db");
 
--- src/update-dictionary.ts
+++ src/update-dictionary.ts
@@ -2,13 +2,13 @@ import fs from "node:fs/promises";
 import fsSync from "node:fs";
 import { Readable } from "node:stream";
 import { finished } from "node:stream/promises";
-import { environment, showToast, Toast } from "@raycast/api";
-import yauzl from "yauzl";
-import { DICTIONARY_PATH, DICTIONARY_URL, DOWNLOAD_PATH } from "./lib";
+import { showToast, Toast } from "@raycast/api";
+import { EXTRACTED_PATH, TEMP_DIR } from "./lib";
+import path from "node:path";
+import { exec } from "node:child_process";
 
 export default async function Command() {
-  throw new Error("This command is a WIP. It is not ready for use yet.");
-  // await updateDictionary();
+  await updateDictionary();
 }
 
 async function updateDictionary() {
@@ -18,23 +18,56 @@ async function updateDictionary() {
     message: `Progress: 0%`,
   });
 
-  await downloadDictionary(toast);
-  await extractDictionary(toast);
-  console.log(environment.supportPath);
+  const gzPath = await downloadFile("http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz", TEMP_DIR, toast);
+  const filePath = await extractDictionary(gzPath!, toast);
+  // console.log(environment.supportPath);
   // await importDictionary(toast);
 }
 
-async function downloadDictionary(toast: Toast) {
+export async function extractDictionary(filePath: string, toast: Toast) {
+  toast.title = "Extracting dictionary...";
+  toast.message = "";
+
+  if (fsSync.existsSync(EXTRACTED_PATH)) {
+    await fs.rm(EXTRACTED_PATH, { recursive: true });
+  }
+
   try {
-    const res = await fetch(DICTIONARY_URL);
+    // Unzip the file
+    await new Promise((resolve) => {
+      exec(`gunzip "${filePath}"`, (err) => {
+        if (err) console.error(err);
+      }).on("exit", async () => {
+        resolve(true);
+      });
+    });
+
+    toast.style = Toast.Style.Success;
+    toast.title = "Dictionary updated successfully";
+    toast.message = "";
+    return path.basename(filePath, ".gz");
+  } catch (error) {
+    console.error("Error extracting dictionary:", error);
+    toast.style = Toast.Style.Failure;
+    toast.title = "Failed to extract dictionary";
+    toast.message = "Please try again later.";
+    return null;
+  }
+}
+
+async function downloadFile(url: string, destination: string, toast: Toast) {
+  try {
+    const res = await fetch(url);
     if (!res.body) throw new Error("Failed to fetch dictionary: No response body");
 
     const contentLength = res.headers.get("content-length");
     const totalBytes = contentLength ? parseInt(contentLength, 10) : 0;
 
     let downloadedBytes = 0;
-    console.log("Downloading to", DOWNLOAD_PATH);
-    const fileStream = fsSync.createWriteStream(DOWNLOAD_PATH, { flags: "w" });
+    const filename = path.basename(url);
+    const destinationPath = path.join(destination, filename);
+    console.log("Downloading to", destinationPath);
+    const fileStream = fsSync.createWriteStream(destinationPath, { flags: "w" });
     const readableStream = Readable.fromWeb(res.body);
 
     // Log the download progress
@@ -46,60 +79,11 @@ async function downloadDictionary(toast: Toast) {
     });
 
     await finished(readableStream.pipe(fileStream));
+    return destinationPath;
   } catch (error) {
     console.error("Error downloading dictionary:", error);
     toast.style = Toast.Style.Failure;
     toast.title = "Failed to download dictionary";
-    toast.message = "Please try again later.";
-  }
-}
-
-// Extract the .mdx file from the downloaded zip file
-export async function extractDictionary(toast: Toast) {
-  try {
-    if (!fsSync.existsSync(DOWNLOAD_PATH)) {
-      toast.style = Toast.Style.Failure;
-      toast.title = "Dictionary download failed";
-      toast.message = "Downloaded file not found. Please try again.";
-      return;
-    }
-
-    if (!fsSync.existsSync(DICTIONARY_PATH)) {
-      await fs.unlink(DICTIONARY_PATH);
-    }
-
-    await new Promise<void>((resolve, reject) => {
-      yauzl.open(DOWNLOAD_PATH, { lazyEntries: true }, (err, zipfile) => {
-        if (err) return reject(err);
-
-        zipfile.readEntry();
-        zipfile.on("entry", (entry) => {
-          if (/\.mdx$/.test(entry.fileName)) {
-            zipfile.openReadStream(entry, (err, readStream) => {
-              if (err) return reject(err);
-
-              const writeStream = fsSync.createWriteStream(DICTIONARY_PATH, { flags: "w" });
-              readStream.pipe(writeStream);
-              writeStream.on("finish", () => {
-                zipfile.close();
-                toast.style = Toast.Style.Success;
-                toast.title = "Dictionary updated successfully";
-                toast.message = "";
-                resolve();
-              });
-            });
-          } else {
-            zipfile.readEntry();
-          }
-        });
-
-        zipfile.on("error", reject);
-      });
-    });
-  } catch (error: unknown) {
-    console.error("Error extracting dictionary:", error);
-    toast.style = Toast.Style.Failure;
-    toast.title = "Failed to extract dictionary";
     toast.message = "Please try again later.";
   }
 }
