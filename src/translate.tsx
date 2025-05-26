25ee630a7a18d65b8a15bc765a94a36154e9288e
Matan Kushner <hello@matchai.dev>
2025-05-26 14:48:38 +0900

    Try using detail metadata for dictionary entries

--- src/translate.tsx
+++ src/translate.tsx
@@ -2,10 +2,10 @@ import { List } from "@raycast/api";
 import { DB_PATH } from "./lib";
 import { useState } from "react";
 import { useSQL } from "@raycast/utils";
-import Turndown from "turndown";
-import { Node, NodeType, parse } from "node-html-parser";
+// import Turndown from "turndown";
+import { HTMLElement, Node, NodeType, parse } from "node-html-parser";
 
-const turndownService = new Turndown();
+// const turndownService = new Turndown();
 
 type KanjiItem = {
   id: string;
@@ -14,7 +14,7 @@ type KanjiItem = {
 };
 
 export default function Command() {
-  const [searchText, setSearchText] = useState<string>("林檎");
+  const [searchText, setSearchText] = useState<string>("うち");
   const { isLoading, data } = useSQL<KanjiItem>(DB_PATH, getKanjiQuery(searchText));
 
   const formattedData = data?.map(formatKanjiItem) || [];
@@ -39,6 +39,25 @@ export default function Command() {
                 <List.Item.Detail.Metadata>
                   <List.Item.Detail.Metadata.Label title="Kanji" text={item.kanji} />
                   <List.Item.Detail.Metadata.Label title="Kana" text={item.kana} />
+                  <List.Item.Detail.Metadata.Separator />
+                  <List.Item.Detail.Metadata.Label
+                    title="Definitions"
+                    text={
+                      item.definitions?.map((definition) => definition.trim()).join(" | ") || "No definitions found"
+                    }
+                  />
+                  {/* {item.definitions?.map((definition, index) => (
+                    <List.Item.Detail.Metadata.Label key={index} title={`${index + 1}.`} text={definition} />
+                  ))} */}
+                  {item.footnoteLinks.length > 0 && <List.Item.Detail.Metadata.Separator />}
+                  {item.footnoteLinks.map((link) => (
+                    <List.Item.Detail.Metadata.Link
+                      key={link.title}
+                      title={link.title}
+                      text={link.text}
+                      target={link.url}
+                    />
+                  ))}
                 </List.Item.Detail.Metadata>
               }
             />
@@ -66,12 +85,32 @@ function formatKanjiItem(item: KanjiItem) {
     .map((node: Node) => node.text)
     .join("");
 
+  const definitions = html.querySelector(".glossary")?.childNodes.map((node) => node.text);
+
+  const footnoteLinks = html
+    .querySelectorAll(".entry-footnotes a")
+    .filter(isValidLink)
+    .map((link) => ({
+      title: link.text,
+      text: kanji || kana,
+      url: link.getAttribute("href")!,
+    }));
+
   return {
     ...item,
     kana,
     kanji,
-    detail: turndownService.turndown(item.paraphrase),
+    definitions,
+    footnoteLinks,
   };
+}
+
+function isValidLink(link: HTMLElement) {
+  if (!link.getAttribute("href")) return false;
+
+  const validSites = ["edrdg.org"];
+  const href = link.getAttribute("href")!;
+  return validSites.some((site) => href.includes(site));
 }
 
 function getKanjiQuery(query: string) {
